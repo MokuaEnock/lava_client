6cc899f258dcddc7a55a2b6df78602536e5a5dfb
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-28 06:32:52 +0300

    add additional components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/pages/navbar";
+import Landing from "./components/pages/landing/landiing";
 
 function App() {
-  return <>App Js</>
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/pages/navbar.js
+++ src/components/pages/navbar.js
@@ -1,3 +1,14 @@
-export default function Navbar() {
-  return <header>Navbar</header>;
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <header className="navbar">
+      <span>Pumpkin</span>
+      <NavLink to="/">Home</NavLink>
+      <NavLink></NavLink>
+      <NavLink></NavLink>
+      <NavLink></NavLink>
+    </header>
+  );
 }
