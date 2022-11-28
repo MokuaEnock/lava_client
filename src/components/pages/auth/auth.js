472f49d9c4cf3fb531d0cb91e82d5e72e20be0f1
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-28 12:00:20 +0300

    create form skeleton

--- src/components/pages/auth/auth.js
+++ src/components/pages/auth/auth.js
@@ -4,8 +4,19 @@ import "./auth.css";
 export default function Authentication() {
   return (
     <main id="authentication">
-      <form className="auth-forms" id="form-get-started"></form>
-      <form className="auth-forms" id="form-log-in"></form>
+      <form className="auth-forms" id="form-get-started">
+        <span>Log In</span>
+        <input type="email" name="email" placeholder="Email" />
+        <input type="password" name="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+      <form className="auth-forms" id="form-log-in">
+        <span>Sign Up</span>
+        <input type="email" name="email" placeholder="Email" />
+        <input type="password" name="password" placeholder="Password" />
+        <input type="password" name="password" placeholder="Password" />
+        <button type="submit">Sign Up</button>
+      </form>
     </main>
   );
 }
