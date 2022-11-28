d4cd30beb51cebe2d335bea5d24b905b5fbb3dbd
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-28 11:42:29 +0300

    create auth.css

--- src/components/pages/auth/auth.js
+++ src/components/pages/auth/auth.js
@@ -1,5 +1,11 @@
 import React from "react";
+import "./auth.css";
 
 export default function Authentication() {
-  return <main>Authentication</main>;
+  return (
+    <main id="authentication">
+      <form className="auth-forms" id="form-get-started"></form>
+      <form className="auth-forms" id="form-log-in"></form>
+    </main>
+  );
 }
