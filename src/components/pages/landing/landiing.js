8e3c0201196666d82813a0bbb101e5058fe41ba6
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-28 08:11:08 +0300

    create skeleton for container1

--- src/components/pages/landing/landiing.js
+++ src/components/pages/landing/landiing.js
@@ -1,3 +1,27 @@
+import "./landing.css";
+
 export default function Landing() {
-  return <main>Landing page</main>;
+  // function Container1() {
+  //   <div className="container" id="container1"></div>;
+  // }
+  // function Container2() {
+  //   <div className="container" id="container2">
+  //     container 2
+  //   </div>;
+  // }
+
+  return (
+    <main className="landing">
+      <div className="container" id="container1">
+        <h1>Give Up</h1>
+        <p>It's too late to find Love</p>
+        <h1>Love</h1>
+        <p>Let's do the finding for you</p>
+        <button>Get Started</button>
+      </div>
+      <div className="container" id="container2">
+        container 2
+      </div>
+    </main>
+  );
 }
